a911abed8bd8b1a9328d8f0a9359c70429d54e3e
Joona Kemppainen <joona.kemppainen@rokki.net>
2019-10-02 21:10:39 +0300

    4.5 valmis, 4.6 ja 4.7 puuttuu

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -8,7 +8,19 @@ const totalLikes = blogs => {
   return blogs.reduce(reducer, 0);
 };
 
+const favoriteBlog = blogs => {
+  const likes = blogs.map(blog => blog.likes);
+  const max = Math.max(...likes);
+  const favorite = blogs.find(blog => blog.likes === max);
+  const result = {};
+  result.title = favorite.title;
+  result.author = favorite.author;
+  result.likes = favorite.likes;
+  return result;
+};
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 };
