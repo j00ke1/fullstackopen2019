5362feed6ac923bd2cfcab345100cc5c2c6044ae
Joona Kemppainen <joona.kemppainen@rokki.net>
2019-10-20 17:53:34 +0300

    5.6 valmis

--- osa5/src/components/Blog.js
+++ osa5/src/components/Blog.js
@@ -1,8 +1,38 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react';
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const blogStyle = {
+    padding: 5,
+    border: 'solid',
+    borderWidth: 1,
+    margin: 5,
+    borderRadius: '5px'
+  };
+
+  if (expanded) {
+    return (
+      <div style={blogStyle}>
+        <div onClick={() => setExpanded(!expanded)}>
+          {blog.title} by {blog.author}
+          <br />
+          <a href={blog.url}>{blog.url}</a>
+          <br />
+          {blog.likes} likes <button>Like</button>
+          <br />
+          Added by {blog.user.name}
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div style={blogStyle}>
+      <div onClick={() => setExpanded(!expanded)}>
+        {blog.title} by {blog.author}
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
